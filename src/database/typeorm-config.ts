8c7f2bb8cb7a1faf5bdfb85b1670bab5148ef98d
sadrack404 <gabrielscr2023@gmail.com>
2023-09-21 22:59:11 -0300

    Adicionado o tema adminlite e seus arquivos para base do front end

---
+++ src/database/typeorm-config.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist';
+import { join } from 'path';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      database: 'ediaristas',
+      username: 'root',
+      password: 'password',
+      host: 'localhost',
+      port: 3306,
+      synchronize: false,
+      type: 'mysql',
+      entities: [join(__dirname, '..', '**type/*entity.{ts,js}')],
+      migrations: [join(__dirname, '..', './database/migrations/*{ts,js}')],
+      namingStrategy: new SnakeNamingStrategy(),
+    };
+  }
+}
