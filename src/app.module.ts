47827e8531dc7dcfdbb9b316f813d529da823820
sadrack101 <gabrielscr2023@gmail.com>
2023-09-22 15:35:46 -0300

    adicionado o modulo servicos ao projeto atraves de comando cli e adicionado os atributos da classe de entidade de servicos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './database/typeorm-config';
+import { ServicosModule } from './servicos/servicos.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
     }),
+    ServicosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
